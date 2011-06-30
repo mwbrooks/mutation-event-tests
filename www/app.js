603e227dbe8d7cd0eb8bfd1bf99a3233b6f62fed
Michael Brooks <michael@michaelbrooks.ca>
2011-06-30 16:17:16 -0700

    Initial commit

---
+++ www/app.js
@@ -0,0 +1,7 @@
+(function(window) {
+
+    var menu = document.createElement('menu');
+    menu.setAttribute('type',  'toolbar');
+    menu.setAttribute('label', 'HTMLMenuElement Demo');
+
+})(window);
