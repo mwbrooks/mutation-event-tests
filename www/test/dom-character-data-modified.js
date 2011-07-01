2c97dab62b981e5e74b95cd3bf95208c7cc916e6
Michael Brooks <michael@michaelbrooks.ca>
2011-06-30 18:04:43 -0700

    Update DOMCharacterDataModified with event data tests.

--- www/test/dom-character-data-modified.js
+++ www/test/dom-character-data-modified.js
@@ -1,12 +1,18 @@
 module('DOMCharacterDataModified', $.config.module);
 
-test('should dispatch DOMCharacterDataModified event', 1, function() {
+test('should dispatch DOMCharacterDataModified event', 2, function() {
     stop($.config.timeout);
 
     $.el.fixture.addEventListener('DOMCharacterDataModified', $.fn.eventListenerCallback, false);
 
     $.fn.callback = function(e) {
         ok(true, 'DOMCharacterDataModified was fired');
+
+        equal(e.target,      el.childNodes[0],     'e.target is the modified element');
+        equal(e.relatedNode, $.el.fixture,         'e.relatedNode should be the parent of the modified element');
+        equal(e.prevValue,   'Some data',          'e.prevValue should be correct');
+        equal(e.newValue,    'Some dataMore data', 'e.newValue should be correct');
+
         start();
     };
 
