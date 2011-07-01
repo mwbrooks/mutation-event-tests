546da571eba677758f6b2f4f296644535a8c46af
Michael Brooks <michael@michaelbrooks.ca>
2011-06-30 17:47:50 -0700

    Add DOMAttrModified.

---
+++ www/test/dom-attr-modified.js
@@ -0,0 +1,17 @@
+module('DOMAttrModified', $.config.module);
+
+test('should dispatch DOMAttrModified event', 1, function() {
+    stop($.config.timeout);
+
+    $.el.fixture.addEventListener('DOMAttrModified', $.fn.eventListenerCallback, false);
+
+    $.fn.callback = function(e) {
+        ok(true, 'DOMAttrModified was fired');
+        start();
+    };
+
+    var el = document.createElement('div');
+    $.el.fixture.appendChild(el);
+    el.setAttribute('id', 'attr-modified-node');
+    el.setAttribute('id', 'attr-modified-node-2');
+});
